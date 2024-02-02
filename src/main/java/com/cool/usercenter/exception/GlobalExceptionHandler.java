9fa1580afaf2f42653750f75e8e726d9f5f5d3f9
Cool <990299103@qq.com>
2024-02-02 23:06:10 +0800

    first init

---
+++ src/main/java/com/cool/usercenter/exception/GlobalExceptionHandler.java
@@ -0,0 +1,32 @@
+package com.cool.usercenter.exception;
+
+import com.cool.usercenter.common.BaseResponse;
+import com.cool.usercenter.common.ErrorCode;
+import com.cool.usercenter.common.ResultUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+/**
+ * 全局异常处理器
+ *
+
+ */
+@RestControllerAdvice
+@Slf4j
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(BusinessException.class)
+    public BaseResponse<?> businessExceptionHandler(BusinessException e) {
+        log.error("businessException: " + e.getMessage(), e);
+        return ResultUtils.error(e.getCode(), e.getMessage(), e.getDescription());
+    }
+
+    //https://github.com/licool
+
+    @ExceptionHandler(RuntimeException.class)
+    public BaseResponse<?> runtimeExceptionHandler(RuntimeException e) {
+        log.error("runtimeException", e);
+        return ResultUtils.error(ErrorCode.SYSTEM_ERROR, e.getMessage(), "");
+    }
+}
---
+++ src/main/java/com/cool/usercenter/mapper/UserMapper.java
@@ -0,0 +1,17 @@
+package com.cool.usercenter.mapper;
+
+import com.cool.usercenter.model.domain.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 用户 Mapper
+ *
+
+ */
+public interface UserMapper extends BaseMapper<User> {
+
+}
+
+
+// [编程知识星球](https://cool.icu) 零基础快速入门编程，不走弯路！30+ 原创学习路线和专栏、1000+ 编程精华文章、500+ 编程学习指南、20T+ 编程资源汇总
+
