1c0ac4d20b0a6e4854d006302dceac324ac4bb45
Cool <990299103@qq.com>
2024-02-10 22:14:54 +0800

    easy excel开发，导入数据

--- src/main/java/com/cool/iku/config/SwaggerConfig.java
+++ src/main/java/com/cool/iku/config/SwaggerConfig.java
@@ -2,6 +2,7 @@ package com.cool.iku.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
@@ -9,54 +10,38 @@ import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
- * 自定义 Swagger 接口文档的配置
+ * 自定义 Knife4j 接口文档的配置
  */
-@Configuration // 配置类
-@EnableSwagger2 // 开启 swagger2 的自动配置
+@Configuration
+@EnableSwagger2WebMvc
+//@Profile({"dev", "test"})   //版本控制访问
 public class SwaggerConfig {
-    @Bean
-    public Docket docket() {
-        // 创建一个 swagger 的 bean 实例
+
+    @Bean(value = "defaultApi2")
+    public Docket defaultApi2() {
         return new Docket(DocumentationType.SWAGGER_2)
-
-                // 配置接口信息
-                .select() // 设置扫描接口
-                // 配置如何扫描接口
-                .apis(RequestHandlerSelectors
-                                //.any() // 扫描全部的接口，默认
-                                //.none() // 全部不扫描
-                                .basePackage("com.cool.iku.controller") // 扫描指定包下的接口，最为常用
-                        //.withClassAnnotation(RestController.class) // 扫描带有指定注解的类下所有接口
-                        //.withMethodAnnotation(PostMapping.class) // 扫描带有只当注解的方法接口
-
-                )
-                .paths(PathSelectors
-                                .any() // 满足条件的路径，该断言总为true
-                        //.none() // 不满足条件的路径，该断言总为false（可用于生成环境屏蔽 swagger）
-                        //.ant("/user/**") // 满足字符串表达式路径
-                        //.regex("") // 符合正则的路径
-                )
+                .apiInfo(apiInfo())
+                .select()
+                // 这里一定要标注你控制器的位置
+                .apis(RequestHandlerSelectors.basePackage("com.cool.iku.controller"))
+                .paths(PathSelectors.any())
                 .build();
     }
 
-    // 基本信息设置
+    /**
+     * api 信息
+     * @return
+     */
     private ApiInfo apiInfo() {
-        Contact contact = new Contact(
-                "Cool", // 作者姓名
-                "https://www.yuque.com/icu0", // 作者网址
-                "cooltest@qq.com"); // 作者邮箱
         return new ApiInfoBuilder()
-                .title("爱酷伙伴匹配系统-接口文档") // 标题
-                .description("众里寻他千百度，慕然回首那人却在灯火阑珊处") // 描述
-                .termsOfServiceUrl("https://www.baidu.com") // 跳转连接
-                .version("1.0") // 版本
-                .license("Swagger-的使用(详细教程)")
-                .licenseUrl("https://blog.csdn.net/xhmico/article/details/125353535")
-                .contact(contact)
+                .title("爱酷伙伴匹配系统")
+                .description("爱酷伙伴匹配系统接口文档")
+                .termsOfServiceUrl("https://gitee.com/xiao-chenago")
+                .contact(new Contact("cool","https://www.yuque.com/icu0","xxx@qq.com"))
+                .version("1.0")
                 .build();
     }
-
 }
---
+++ src/main/java/com/cool/iku/once/ImportExcel.java
@@ -0,0 +1,46 @@
+package com.cool.iku.once;
+
+import com.alibaba.excel.EasyExcel;
+
+import java.util.List;
+
+/**
+ * @Description:    导入Excel，读取数据
+ */
+public class ImportExcel {
+    /**
+     * 读取数据
+     */
+    public static void main(String[] args) {
+        // 写法1：JDK8+ ,不用额外写一个DemoDataListener
+        // since: 3.0.0-beta1
+        //Excel数据文件放在自己电脑上，能够找到的路径
+        String fileName = "D:\\Cool\\Desktop\\伙伴匹配\\iku-backend\\src\\main\\resources\\testExcel.xlsx";
+//          readByListener(fileName);
+        synchronousRead(fileName);
+
+    }
+    /**
+     * 监听器读取
+     * @param fileName
+     */
+    public static void readByListener(String fileName) {
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 文件流会自动关闭
+        // 这里每次会读取100条数据 然后返回过来 直接调用使用数据就行
+        EasyExcel.read(fileName, XingQiuTableUserInfo.class, new TableListener()).sheet().doRead();
+    }
+
+    /**
+     * 同步读
+     * 同步的返回，不推荐使用，如果数据量大会把数据放到内存里面
+     */
+    public static void synchronousRead(String fileName) {
+        // 这里 需要指定读用哪个class去读，然后读取第一个sheet 同步读取会自动finish
+        //这里返回的list为一个列表，需要遍历展示单个数据
+        List<XingQiuTableUserInfo> list = EasyExcel.read(fileName).head(XingQiuTableUserInfo.class).sheet().doReadSync();
+        for (XingQiuTableUserInfo xingQiuTableUserInfo : list) {
+            System.out.println(xingQiuTableUserInfo);
+        }
+
+    }
+}
