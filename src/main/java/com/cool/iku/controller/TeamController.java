725595e308844c4b40003be2f843297595e1c42b
Cool <990299103@qq.com>
2024-08-04 15:23:13 +0800

    fix:"日期格式修改，跨域问题修改"

--- src/main/java/com/cool/iku/controller/TeamController.java
+++ src/main/java/com/cool/iku/controller/TeamController.java
@@ -31,7 +31,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/team")
-@CrossOrigin(origins = {"http://localhost:5173/"})
+@CrossOrigin(origins = {"http://localhost:3000"},allowCredentials = "true")
 @Slf4j
 public class TeamController {
 
